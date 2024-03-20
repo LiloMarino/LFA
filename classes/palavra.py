6d92b1b295a96a17f94220933be914f2ccf3b5c1
Murilo <lilo.marino@hotmail.com>
2024-03-20 17:01:18 -0300

    Criado classe Palavra

---
+++ classes/palavra.py
@@ -0,0 +1,52 @@
+from classes.alfabeto import Alfabeto
+
+
+class Palavra:
+    def __init__(self, alfabeto: Alfabeto, palavra: str = "") -> None:
+        """
+        Instancia uma Palavra
+
+        Args:
+            alfabeto (Alfabeto): Alfabeto usado na construção da palavra
+            palavra (str, optional): Palavra. Defaults to "".
+
+        Raises:
+            ValueError: Caso o símbolo da palavra não esteja no alfabeto
+        """
+        for simbolo in palavra:
+            if simbolo not in alfabeto:
+                raise ValueError(f"O símbolo '{simbolo}' não está no alfabeto.")
+        self.palavra = palavra
+        self.alfabeto = alfabeto
+
+    # Métodos Mágicos
+
+    def __len__(self) -> int:
+        return self.palavra.__len__()
+
+    def __add__(self, other: object) -> "Palavra":
+        if isinstance(other, Palavra):
+            if other.alfabeto == self.alfabeto:
+                return Palavra(self.alfabeto, self.palavra + other.palavra)
+            else:
+                raise ValueError(
+                    "Os alfabetos das palavras devem ser iguais para a adição."
+                )
+        elif not isinstance(other, Palavra) and isinstance(other, str):
+            return Palavra(self.alfabeto, self.palavra + other)
+        else:
+            raise TypeError("Só é permitido a concatenação entre palavras e símbolos!")
+
+    def __eq__(self, __value: object) -> bool:
+        return isinstance(__value, Palavra) and self.palavra == __value
+
+    def __str__(self) -> str:
+        if len(self.palavra) == 0:
+            return "λ"
+        else:
+            return f"'{self.palavra}'"
+
+    def __repr__(self) -> str:
+        return (
+            f"Palavra(palavra = '{self.palavra}', alfabeto = {self.alfabeto.simbolos})"
+        )
