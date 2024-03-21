31aea168f380a1be7a8cd0224a47595dc576f890
LiloMarino <lilo.marino@hotmail.com>
2024-03-21 10:27:23 -0300

    feat: Criado classe Gramática e melhorado representação de sintaxe

---
+++ classes/gramatica.py
@@ -0,0 +1,38 @@
+from classes.alfabeto import Alfabeto
+from classes.sintaxe import Sintaxe
+from classes.vocabulario import Vocabulario
+
+
+class Gramatica:
+    def __init__(
+        self,
+        vocabulario: Vocabulario,
+        alfabeto: Alfabeto,
+        sintaxe: Sintaxe,
+        simbolo_inicial: str,
+    ) -> None:
+        assert isinstance(
+            vocabulario, Vocabulario
+        ), "vocabulario deve ser uma instância de Vocabulario"
+        assert isinstance(
+            alfabeto, Alfabeto
+        ), "alfabeto deve ser uma instância de Alfabeto"
+        assert isinstance(sintaxe, Sintaxe), "sintaxe deve ser uma instância de Sintaxe"
+        assert (
+            isinstance(simbolo_inicial, str) and len(simbolo_inicial) == 1
+        ), "simbolo_inicial deve ser uma string com um único caractere"
+        self.alfabeto = alfabeto
+        self.vocabulario = vocabulario
+        self.sintaxe = sintaxe
+        self.simbolo_inicial = simbolo_inicial
+
+    def __str__(self) -> str:
+        return f"G = ({self.vocabulario}, {self.alfabeto}, {self.sintaxe}, S = {self.simbolo_inicial} )"
+
+    def __repr__(self) -> str:
+        return (
+            f"{self.__class__.__name__}(vocabulario={repr(self.vocabulario)}, "
+            f"alfabeto={repr(self.alfabeto)}, "
+            f"sintaxe={repr(self.sintaxe)}, "
+            f'simbolo_inicial="{repr(self.simbolo_inicial)}")'
+        )
