2369dcad3d3e4b2b63b30c8bf4c69c53988ca4c7
LiloMarino <lilo.marino@hotmail.com>
2024-03-21 10:09:13 -0300

    feat: Melhorado sintaxe

--- classes/sintaxe.py
+++ classes/sintaxe.py
@@ -3,95 +3,72 @@ from collections import abc
 from classes.producao import Producao
 
 
-class Sintaxe(abc.Iterable):
+class Sintaxe(abc.MutableMapping):
 
-    def __init__(self, producoes: list[Producao] | Producao) -> None:
+    def __init__(self, prod: dict[str, list[str]] | list[Producao] | Producao) -> None:
         """
         Instancia uma Sintaxe
 
         Args:
-            producoes (list[Producao] | Producao): Produção ou uma
-            lista de produções para a criação da sintaxe
+            prod (dict[str, str] | list[Producao] | Producao): Um dicionário
+            que representa as produções, produção ou
+            uma lista de produções para a criação da sintaxe
 
         Raises:
             TypeError: Caso o argumento seja do tipo inválido
         """
-        if isinstance(producoes, Producao):
-            self.producoes = list()
-            self.producoes.append(producoes)
-        elif isinstance(producoes, list):
-            for prod in producoes:
-                assert isinstance(prod, Producao)
-            self.producoes = producoes
+        self.__producoes: dict[str, list[str]] = dict()
+        if isinstance(prod, Producao):
+            self.__producoes[prod.origem] = prod.produto
+        elif isinstance(prod, list):
+            for p in prod:
+                assert isinstance(
+                    prod, Producao
+                ), "As produções devem  ser instâncias de Producao"
+            for p in prod:
+                if self.__producoes[p.origem]:
+                    self.__producoes[p.origem].append(p.produto)
+                else:
+                    self.__producoes[p.origem] = [p.produto]
+        elif isinstance(prod, dict):
+            for key, value in prod.items():
+                assert isinstance(key, str) and isinstance(
+                    value, list
+                ), f"Chaves e valores devem ser strings; '{type(key).__name__}' e '{type(value).__name__}' encontrados."
+                for i in value:
+                    assert isinstance(
+                        i, str
+                    ), "Os valores do dicionário devem ser strings."
+            self.__producoes = prod
         else:
-            raise TypeError(
-                "Produções deve ser uma produção única ou uma lista de produções"
-            )
-
-    def append(self, object: Producao) -> None:
-        """
-        Adiciona a produção à sintaxe
-
-        Args:
-            object (Producao): Produção a ser adicionada
-        """
-        if isinstance(object, Producao):
-            self.producoes.append(object)
-        else:
-            raise TypeError("Objetos devem ser instâncias da classe 'Producao'")
-
-    def remove(
-        self, producao: Producao = None, origem: str = None, produto: str = None
-    ) -> None:
-        """
-        Remove uma produção da sintaxe
-
-        Args:
-            producao (Producao, optional): Produção a ser removida. Defaults to None.
-            origem (str, optional): Origem da produção. Defaults to None.
-            produto (str, optional): Produto da produção. Defaults to None.
-
-        Raises:
-            ValueError: Caso os argumentos estejam inválidos
-            ValueError: Caso não exista nenhuma produção igual a especificada
-        """
-
-        p = producao if isinstance(producao, Producao) else None
-        if origem is not None and produto is not None:
-            p = Producao(origem, produto)
-        if p is None:
-            raise ValueError("Deve-se informar um par origem/produto ou uma produção")
-
-        for producao in self.producoes:
-            if p == producao:
-                self.producoes.remove(producao)
-                return
-        raise ValueError(
-            f"Não existe nenhuma produção com Origem '{origem}' e Produto '{produto}'"
-        )
-
-    def sort(self, key: None = None, reverse: bool = False) -> None:
-        """
-        Ordena a sintaxe
-
-        Args:
-            key (None, optional): Chave de ordenação. Defaults to None.
-            reverse (bool, optional): Reverso. Defaults to False.
-        """
-        if key is None:
-            self.producoes.sort(key=lambda producao: producao.origem, reverse=reverse)
-        else:
-            self.producoes.sort(key=key, reverse=reverse)
+            raise TypeError(f"Tipo de argumento inválido: {prod}")
 
     # Métodos Mágicos
     def __str__(self) -> str:
-        return str([str(producao) for producao in self.producoes])
+        return str([str(producao) for producao in self.__producoes])
 
     def __repr__(self) -> str:
-        return f"Sintaxe(produções = {self.producoes})"
+        return f"Sintaxe(produções = {self.__producoes})"
 
     def __iter__(self) -> abc.Iterator:
-        return self.producoes.__iter__()
+        return self.__producoes.__iter__()
 
-    def __getitem__(self, origem: str) -> list[Producao]:
-        return Sintaxe([prod for prod in self.producoes if prod.origem == origem])
+    def __len__(self) -> int:
+        return self.__producoes.__len__()
+
+    def __getitem__(self, __key: str) -> list[str]:
+        return self.__producoes[__key]
+
+    def __setitem__(self, __key: str, __value: str | list[str]) -> None:
+        assert isinstance(__key, str), "A chave deve ser uma string"
+        if isinstance(__value, list):
+            for prod in __value:
+                assert isinstance(prod, str), "As produções devem ser strings"
+            self.__producoes[__key] = __value
+        elif isinstance(__value, str):
+            self.__producoes[__key].append(__value)
+        else:
+            raise TypeError("Tipos de valor inválidos")
+
+    def __delitem__(self, __key: str) -> None:
+        return self.__producoes.__delitem__(__key)
