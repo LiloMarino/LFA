23ae1d11f36af14f53119eb48ff7377b19997163
LiloMarino <lilo.marino@hotmail.com>
2024-03-21 10:09:13 -0300

    Melhorado sintaxe

---
+++ classes/__init__.py
@@ -0,0 +1,6 @@
+from classes.alfabeto import Alfabeto
+from classes.linguagem import Linguagem
+from classes.palavra import Palavra
+from classes.producao import Producao
+from classes.sintaxe import Sintaxe
+from classes.vocabulario import Vocabulario
