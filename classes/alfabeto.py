6805e0d15b66f64de13ac87b64c32c5b77a906dd
Murilo <lilo.marino@hotmail.com>
2024-04-16 13:20:21 -0300

    feat: Alterado implementação de Vocabulário e Alfabeto

--- classes/alfabeto.py
+++ classes/alfabeto.py
@@ -1,27 +1,21 @@
 from collections import abc
-from typing import Generator
+from typing import Generator, Iterable
 
 
-class Alfabeto(abc.MutableSet):
-    def __init__(self, simbolos: set[str] | str) -> None:
+class Alfabeto(set):
+    def __init__(self, simbolos: Iterable) -> None:
         """
-        Instancia um Alfabeto
+        Inicia um alfabeto
 
         Args:
-            simbolos (set[str]):  Conjunto de símbolos finitos que formam o alfabeto
+            simbolos (Iterable): Símbolos do alfabeto
 
         Raises:
-            ValueError: Caso o conjunto esteja vazio
-            TypeError: Caso os elementos do conjunto não sejam uma string ou um conjunto
+            ValueError: Caso o alfabeto seja vazio
         """
         if not simbolos:
             raise ValueError("O alfabeto não pode ser vazio")
-        if isinstance(simbolos, set):
-            self.simbolos = simbolos
-        elif isinstance(simbolos, str):
-            self.simbolos = set(simbolos)
-        else:
-            raise TypeError("Símbolos deve ser um conjunto ou uma string")
+        super().__init__(simbolos)
 
     @property
     def simbolos_ordenados(self) -> list:
@@ -31,35 +25,7 @@ class Alfabeto(abc.MutableSet):
         Returns:
             list: Retorna uma lista ordenada dos símbolos
         """
-        return sorted(self.simbolos)
-
-    def add_simbolo(self, simbolo: str) -> None:
-        """
-        Adiciona um símbolo ao alfabeto
-
-        Args:
-            simbolo (str): Símbolo a ser adicionado
-
-        Raises:
-            TypeError: Caso o símbolo não seja um caractere
-        """
-        if isinstance(simbolo, str):
-            self.simbolos.add(simbolo)
-        else:
-            raise TypeError("Simbolo deve ser uma string")
-
-    def discard_simbolo(self, simbolo: str) -> bool:
-        """
-        Remove um símbolo do alfabeto e retorna se foi removido com sucesso
-
-        Args:
-            simbolo (str): Símbolo a ser removido do alfabeto
-
-        Returns:
-            bool: Retorna se o símbolo foi removido com sucesso
-        """
-        self.simbolos.discard(simbolo)
-        return True if simbolo in self.simbolos else False
+        return sorted(self)
 
     def frt(self, max_depth: int = -1) -> Generator:
         """
@@ -135,22 +101,5 @@ class Alfabeto(abc.MutableSet):
 
     # Métodos Mágicos
 
-    def __contains__(self, x: object) -> bool:
-        return self.simbolos.__contains__(x)
-
-    def __iter__(self) -> abc.Iterator:
-        return self.simbolos.__iter__()
-
-    def __len__(self) -> int:
-        return self.simbolos.__len__()
-
-    def __eq__(self, other: object) -> bool:
-        if isinstance(other, Alfabeto):
-            return self.simbolos == other.simbolos
-        return TypeError("É possível comparar apenas entre alfabetos")
-
     def __str__(self) -> str:
         return f"Σ = {self.simbolos_ordenados}"
-
-    def __repr__(self) -> str:
-        return f"Alfabeto(simbolos = {self.simbolos_ordenados})"
--- classes/vocabulario.py
+++ classes/vocabulario.py
@@ -1,52 +1,32 @@
 from collections import abc
+from typing import Iterable
 
 from classes.alfabeto import Alfabeto
 
 
-class Vocabulario(abc.MutableSet):
-    def __init__(
-        self, simbolos_n_terminais: set[str] | str, alfabeto: Alfabeto
-    ) -> None:
+class Vocabulario(set):
+    def __init__(self, simbolos_n_terminais: Iterable, alfabeto: Alfabeto) -> None:
         """
-        Instancia um Vocabulario
+        Inicia um Vocabulário
 
         Args:
-            simbolos (set[str]):  Conjunto de símbolos terminais e não terminais finitos
-            que formam o vocabulário
+            simbolos_n_terminais (Iterable): Símbolos não terminais
+            alfabeto (Alfabeto): Alfabeto
 
         Raises:
-            ValueError: Caso o conjunto esteja vazio
-            TypeError: Caso os elementos do conjunto não sejam uma string ou um conjunto
+            ValueError: Caso exista alguma intersecção entre o alfabeto e os símbolos não terminais
         """
-        if isinstance(simbolos_n_terminais, str):
-            simbolos_n_terminais = set(simbolos_n_terminais)
-        if not isinstance(simbolos_n_terminais, set):
-            raise TypeError("Símbolos não terminais deve ser um conjunto ou uma string")
-        intersecao = simbolos_n_terminais & alfabeto.simbolos
-        if intersecao:
+        assert isinstance(
+            alfabeto, Alfabeto
+        ), "O parâmetro 'alfabeto' deve ser do tipo Alfabeto"
+        super().__init__(simbolos_n_terminais)
+        if not self.isdisjoint(alfabeto):
             raise ValueError(
-                f"Os símbolos não terminais e os símbolos terminais têm intersecção: {intersecao}"
+                f"Os símbolos não terminais e os símbolos terminais têm intersecção: {self & alfabeto}"
             )
-        self.vocabulario = simbolos_n_terminais | alfabeto.simbolos
+        self |= alfabeto
 
     # Métodos Mágicos
 
-    def __contains__(self, x: object) -> bool:
-        return self.vocabulario.__contains__(x)
-
-    def __iter__(self) -> abc.Iterator:
-        return self.vocabulario.__iter__()
-
-    def __len__(self) -> int:
-        return self.vocabulario.__len__()
-
-    def __eq__(self, other: object) -> bool:
-        if isinstance(other, Vocabulario):
-            return self.vocabulario == other.vocabulario
-        return TypeError("É possível comparar apenas entre vocabulários")
-
     def __str__(self) -> str:
-        return f"V = {sorted(self.vocabulario)}"
-
-    def __repr__(self) -> str:
-        return f"Vocabulário(vocabulário = {sorted(self.vocabulario)})"
+        return f"V = {sorted(self)}"
